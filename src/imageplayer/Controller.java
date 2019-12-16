30ac6384a491ea38cae260f8de0efd18d6b1fe18
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-12-16 09:44:50 +0100

    added fields

--- src/imageplayer/Controller.java
+++ src/imageplayer/Controller.java
@@ -1,4 +1,27 @@
 package imageplayer;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    public ImageView imageView;
+
+    @FXML
+    public ProgressBar progressBar;
+
+    @FXML
+    public Label statusLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        statusLabel.setText("application started ...");
+    }
 }
